6b059ea3974c86702a804e210abf29367a8a6ef8
Tochsaporn Petchpol <tochsaporn@thelivingos.com>
2023-09-25 16:10:20 +0700

    update

--- app/services/fight_service.py
+++ app/services/fight_service.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import Depends
 from ..repositories.character_repository import CharacterRepository
 from ..dependencies import auth_user_dependency
+from ..dtos.fight_dtos import GetResultDto
 
 
 class FightService:
@@ -12,3 +13,8 @@ class FightService:
                  ):
         self.character_repo = character_repo
         self.auth_user =  auth_user
+        
+    # def start(self) -> GetResultDto:
+    #     result = GetResultDto()
+    #     characters = self.character_repo.read_all(1, 100, self.auth_user.user_id)
+        
